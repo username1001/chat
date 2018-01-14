9a8cd9182b93ae1897c7b8399166153db22f066f
Matt <faymatt96@gmail.com>
2018-01-14 18:03:25 -0500

    Add helmetjs

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var app = require("express")();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
+var helmet = require("helmet");
 var port = process.env.PORT || 3000;
+
+app.use(helmet());
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
