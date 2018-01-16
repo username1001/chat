5c5de4d5fa5ef1749af463eac1963cce087bf550
Matt <faymatt96@gmail.com>
2018-01-16 07:34:20 -0500

    Fix quote

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 3000;
 
 app.use(helmet());
 
-app.get("/", function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
@@ -18,7 +18,7 @@ io.on("connection", function(socket) {
 
 io.clients((error, clients) => {
   if (error) throw error;
-  console.log(clients); // => [6em3d4TJP8Et9EMNAAAA, G5p55dHhGgUnLUctAAAB]
+  console.log(clients);
 });
 
 http.listen(port, function() {
