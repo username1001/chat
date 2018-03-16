ad30a8d1d6c25ef8887ca9dc3084dfb2588ace22
Matt <faymatt96@gmail.com>
2018-03-16 16:59:26 -0400

    Update code

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ const port = process.env.PORT || 3000;
 
 app.use(helmet());
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket) {
+io.on('connection', socket => {
   socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
   });
 });
 
-io.clients((error, clients) => {
-  if (error) throw error;
-  console.log(clients);
-});
+// io.clients((error, clients) => {
+//   if (error) throw error;
+//   console.log(clients);
+// });
 
-http.listen(port, function() {
+http.listen(port, () => {
   console.log('listening on *:' + port);
 });
